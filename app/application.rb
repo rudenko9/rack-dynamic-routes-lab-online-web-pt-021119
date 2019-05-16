f33ec9b6751805a4328d549b5933a57a70f9180b
Nataliya R <n.rudenko@gmail.com>
2019-05-16 21:11:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,14 +6,15 @@ class Application
     req = Rack::Request.new(env)
     
     if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
       
-      @@items.each do |item|
-      resp.write "#{item.price}"
-    end 
-    elsif
+      item = @@items.each {|i| i.name == item_name}
+  
+    if item.nil?
       resp.write  "Item not found"
       resp.status = 400
     else
+      
       resp.write "Route not found"
       resp.status = 404
     end 
