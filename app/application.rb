4cdc422033c7d49bc1c9a2d3b8681341295983b3
Nataliya R <n.rudenko@gmail.com>
2019-05-16 21:07:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
     
-    if req.path.match(/item/)
+    if req.path.match(/items/)
       @@items.each do |item|
       resp.write "#{item.price}"
     end 
