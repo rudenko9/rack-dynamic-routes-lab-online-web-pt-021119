62713a3f1872d3ac677de1ef4f9e5e3a775e0efc
Nataliya R <n.rudenko@gmail.com>
2019-05-16 20:53:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,14 @@
 class Application
-  @@item = []
+ # @@item = []
   
   def call(env)
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
     
     if req.path=="/item"
+      resp.write "item price"
+    else 
+      
     
     
   end
