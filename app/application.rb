22f3b428d25641532ad01f7ff6dcee7b2b65befc
Nataliya R <n.rudenko@gmail.com>
2019-05-16 20:58:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,7 +11,7 @@ class Application
     else 
       resp.write  "Item not found"
       resp.write "Route not found"
-      resp.status = 400
+      resp.status = 404
     end 
     resp.finish
   end
