92eeace062a5acac285dc718ec9bb6d04d5abd78
Nataliya R <n.rudenko@gmail.com>
2019-05-16 20:54:46 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+ # @@item = []
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    if req.path=="/item"
+      resp.write "item price"
+    else 
+      resp.write "Route not found"
+      resp.status = 404
+    end 
+    resp.finish
+  end
+  
+end 
